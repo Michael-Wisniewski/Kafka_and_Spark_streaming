94518fb6c96cf848bd3dfdcf4d3737072ba1af92
Michał Wiśniewski <mikex929@gmail.com>
2022-06-19 20:12:29 +0200

    First working version.

---
+++ producer/src/main/java/producer/AppConfigs.java
@@ -0,0 +1,8 @@
+package producer;
+
+public class AppConfigs {
+    final static String applicationID = "Producer";
+    final static String bootstrapServers = "broker:9092";
+    final static String topicName = "test-topic";
+    final static Integer messageMillisecondsDelay = 1000;
+}
--- producer/src/main/java/producer/Main.java
+++ producer/src/main/java/producer/Main.java
@@ -1,8 +1,50 @@
 package producer;
 
-public class Main {
-    public static void main(String[] args) {
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 
-        while(true) System.out.println("Hello from producer");
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+//    private static final Logger logger = LogManager.getLogger();
+    private static void wait(int ms)
+    {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAA");
+//        logger.info("Starting Kafka Producer...");
+        Properties props = new Properties();
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, AppConfigs.applicationID);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<Integer, String> producer = new KafkaProducer<Integer, String>(props);
+
+        int i = 0;
+        while(true) {
+            producer.send(new ProducerRecord<>(AppConfigs.topicName, i,"Simple message " + i));
+            System.out.println("Sended message " + i);
+            i++;
+            wait(AppConfigs.messageMillisecondsDelay);
+        }
+
+
     }
 }
